7c8ab16a08890db8fde27484c6c317e3909a1f71
nikkithanithya <nikkithanithya2@gmail.com>
2022-11-14 14:28:34 +0530

    change1

---
+++ Documents/Nithya/scalar.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:14:43 2022
+
+@author: cse17
+"""
+
+numbers_of_unit=50
+mass_of_sum=1.989e30
+length_great_wall_of_china=21196
+print(numbers_of_unit)
+print(mass_of_sum)
+print(length_great_wall_of_china)
+
